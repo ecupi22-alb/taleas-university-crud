b385678f25331bc0c3df080c21899a7acfc3c11d
Emiliano Cupi <ecupi22@gmail.com>
2026-02-12 14:41:21 +0100

    chore: remove top-level duplicate files and workflows, keep backend/ and frontend/

--- routes/enrollments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const enrollmentController = require('../controllers/enrollmentController');
-
-router.get('/', enrollmentController.getAll);
-router.post('/', enrollmentController.enroll);
-router.delete('/', enrollmentController.unenroll);
-
-module.exports = router;
